a55e256862ccc35bbaaf64d3aa68ff113f377882
Dalton Fox <foxd2@mymail.shawnee.edu>
2019-01-26 00:31:35 -0500

    Added 'shooting'
    - Sure here's a commit note

---
+++ GGJ-2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody m_Rigidbody;
+    public float max_speed;
+    public float acceleration;
+    private Vector3 movement;
+    private float x, y = 0;
+
+    public float projectileSpeed;
+    public GameObject projectilePrefab;
+
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_Rigidbody.velocity.magnitude < max_speed)
+            m_Rigidbody.AddForce(movement * acceleration);
+    }
+
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        movement = new Vector3(x, y, 0);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+            Vector2 direction = target - myPos;
+            direction.Normalize();
+            GameObject projectile = (GameObject) Instantiate(projectilePrefab, myPos, Quaternion.identity);
+            projectile.GetComponent<Rigidbody>().velocity = direction * projectileSpeed;
+
+            Vector2 mousePos = Input.mousePosition;
+            Vector3 screenPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, transform.position.z - Camera.main.transform.position.z));
+            projectile.transform.RotateAroundLocal(new Vector3(0, 0, 1), Mathf.Atan2(screenPos.y - transform.position.y, screenPos.x - transform.position.x));
+        }
+
+    }
+}
---
+++ GGJ-2019/Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static explicit operator GameObject(Projectile v)
+    {
+        throw new NotImplementedException();
+    }
+}
